c2f49155b2de864bf7faf7384e4d5165410489f1
barandiaye-jpg <bara.ndiaye@hec.ca>
2026-02-07 19:53:47 -0500

    Update app.py

--- ai-backend/app.py
+++ ai-backend/app.py
@@ -107,12 +107,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
+        "https://techshop-rmo8.onrender.com",  # ton site statique
+        "http://localhost:5500",               # optionnel (tests locaux)
+        "http://127.0.0.1:5500"                # optionnel (tests locaux)
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
